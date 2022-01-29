2bc866bf1191e853f690963c75c3c4fb2faa7e71
jisun94 <jisun94lee@gmail.com>
2022-01-29 19:02:21 +0900

    ✨ Mood Diary - Complete Home page

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,13 +1,26 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DiaryItem from './DiaryItem';
+import MyButton from './MyButton';
 
 const sortOptionList = [
   { value: 'newest', name: 'Newest' },
   { value: 'oldest', name: 'Oldest' },
 ];
 
+const filterOptionList = [
+  { value: 'all', name: 'All' },
+  { value: 'good', name: 'Good' },
+  { value: 'bad', name: 'Bad' },
+];
+
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
       {optionList.map((it, idx) => (
         <option value={it.value} key={idx}>
           {it.name}{' '}
@@ -18,9 +31,19 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 };
 
 const DiaryList = ({ diaryList }) => {
-  const [sortType, setSortType] = useState('latest');
+  const navigate = useNavigate();
+  const [sortType, setSortType] = useState('newest');
+  const [filter, setFilter] = useState('all');
 
   const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === 'good') {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
     const compare = (a, b) => {
       if (sortType === 'newest') {
         return parseInt(b.date) - parseInt(a.date);
@@ -29,19 +52,39 @@ const DiaryList = ({ diaryList }) => {
       }
     };
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare);
+
+    const filteredList =
+      filter === 'all' ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   };
 
   return (
-    <div>
-      <ControlMenu
-        value={sortType}
-        onChange={setSortType}
-        optionList={sortOptionList}
-      />
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            type={'positive'}
+            text={'New Diary'}
+            onClick={() => navigate('./new')}
+          />
+        </div>
+      </div>
+
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <DiaryItem key={it.id} {...it} />
       ))}
     </div>
   );
