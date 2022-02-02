810477c60378b9922fc1a98eb78f8b337e37e864
jisun94 <jisun94lee@gmail.com>
2022-02-02 20:48:47 +0900

    ✨ Mood Diary - Implement diary edit function

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,36 +1,32 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryStateContext } from '../App';
+import DiaryEditor from '../components/DiaryEditor';
 
 const Edit = () => {
+  const [originData, setOriginData] = useState();
   const navigate = useNavigate();
+  const { id } = useParams();
 
-  const [searchParmas, setSearchParams] = useSearchParams();
+  const diaryList = useContext(DiaryStateContext);
 
-  const id = searchParmas.get('id');
-  console.log('id :', id);
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      console.log(targetDiary);
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate('/', { replace: true });
+      }
+    }
+  }, [id, diaryList]);
 
-  const mode = searchParmas.get('mode');
-  console.log('mode :', mode);
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 일기 수정 페이지 입니다.</p>
-      <button onClick={() => setSearchParams({ id: 100, mode: 'dark' })}>
-        QS바꾸기
-      </button>
-      <button
-        onClick={() => {
-          navigate('/home');
-        }}
-      >
-        go to home
-      </button>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        뒤로가기
-      </button>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
