202fe33959bb2290e783e1a23ecabe24bcae8e44
jisun94 <jisun94lee@gmail.com>
2022-02-03 01:16:58 +0900

    ✨ Mood Diary - Make diary detail page

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -6,41 +6,11 @@ import MyHeader from './MyHeader';
 import MyButton from './MyButton';
 import EmotionItem from './EmotionItem';
 
+import { getStringDate } from '../util/date';
+import { emotionList } from '../util/emotion';
+
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || '';
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion/emotion1.png`,
-    emotion_description: '완전 좋음',
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion/emotion2.png`,
-    emotion_description: '좋음',
-  },
-
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion/emotion3.png`,
-    emotion_description: '그럭저럭',
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion/emotion4.png`,
-    emotion_description: '나쁨',
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion/emotion5.png`,
-    emotion_description: '끔찍함',
-  },
-];
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
